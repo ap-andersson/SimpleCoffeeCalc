a47c3cc693b83475156ce6be1141c4e022149f01
Andreas Andersson <me@andersson-andreas.se>
2024-12-28 18:31:32 +0100

    Making it work

--- app/src/main/java/se/andynet/simplecoffeecalc/MainActivity.kt
+++ app/src/main/java/se/andynet/simplecoffeecalc/MainActivity.kt
@@ -13,6 +13,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.widget.doAfterTextChanged
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "datastore_preferences")
 
 class MainActivity : AppCompatActivity() {
 
@@ -77,6 +88,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Writing on the file
+    private suspend fun savePreference(ratio: Int, isWater: Boolean, weight: Int?) {
+        val ratioKey = intPreferencesKey("ratio")
+        val isWaterKey = booleanPreferencesKey("isWater")
+        val weightKey = intPreferencesKey("weight")
+
+        var actualWeight = 0;
+        if(weight != null) {
+            actualWeight = weight.toInt()
+        }
+
+        dataStore.edit {
+            it[ratioKey] = ratio
+            it[isWaterKey] = isWater
+            it[weightKey] = actualWeight
+        }
+    }
+
+    // Reading the file
+    private suspend fun readPreference(): Settings {
+        val ratioKey = intPreferencesKey("ratio")
+        val isWaterKey = booleanPreferencesKey("isWater")
+        val weightKey = intPreferencesKey("weight")
+
+        val pref = dataStore.data.first()
+
+        return Settings(pref[ratioKey], pref[isWaterKey], pref[weightKey])
+    }
+
     fun calculate() {
         val waterOrBeansSpinner: Spinner = findViewById(R.id.waterOrBeansDropdown)
         val ratioSpinner: Spinner = findViewById(R.id.ratioDropdown)
@@ -84,24 +124,30 @@ class MainActivity : AppCompatActivity() {
         val resultTextLong: TextView = findViewById(R.id.resultTextLong)
         val resultTextShort: TextView = findViewById(R.id.resultTextShort)
 
-        val ratioSelected = resources.getStringArray(R.array.ratioValues)[ratioSpinner.selectedItemPosition].toInt().toDouble()
+        val ratioSelected = resources.getStringArray(R.array.ratioValues)[ratioSpinner.selectedItemPosition].toInt()
         val measuredWeight = gramsInputNumber.text.toString().toIntOrNull()
+        val isWater = waterOrBeansSpinner.selectedItemPosition == 0
+
+        lifecycleScope.launch {
+            savePreference(ratioSelected, isWater, measuredWeight)
+        }
 
         if (measuredWeight == null) {
             resultTextShort.text = ""
             resultTextLong.text = ""
         } else {
-            val isWater = waterOrBeansSpinner.selectedItemPosition == 0
 
             if (isWater) {
-                var amountOfBeans: Int = (measuredWeight.toDouble() * (1/ratioSelected)).toInt()
+                var amountOfBeans: Int = (measuredWeight.toDouble() * (1/ratioSelected.toDouble())).toInt()
                 resultTextShort.text = "$amountOfBeans grams"
                 resultTextLong.text = "For $measuredWeight grams of water you need \n$amountOfBeans grams of beans"
             } else {
-                var amountOfWater: Int = (measuredWeight.toDouble() * ratioSelected).toInt()
+                var amountOfWater: Int = (measuredWeight.toDouble() * ratioSelected.toDouble()).toInt()
                 resultTextShort.text = "$amountOfWater grams"
                 resultTextLong.text = "For $measuredWeight grams of beans you need \n$amountOfWater grams of water"
             }
         }
     }
 }
+
+data class Settings(val ratio: Int?, val isWater: Boolean?, val weight: Int?)
